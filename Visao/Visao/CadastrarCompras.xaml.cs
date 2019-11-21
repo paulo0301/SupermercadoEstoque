7b74c422a91ad1255d4bef50c70d73d89f3ae298
Paulo Daniel Carvalho de Souza <paulo.daniel@academico.ifrn.edu.br>
2019-11-21 20:09:30 -0300

    11

---
+++ Visao/Visao/CadastrarCompras.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Visao
+{
+    /// <summary>
+    /// Lógica interna para CadastrarCompras.xaml
+    /// </summary>
+    public partial class CadastrarCompras : Window
+    {
+        public CadastrarCompras()
+        {
+            InitializeComponent();
+        }
+    }
+}
